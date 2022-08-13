0f74dd71574e5dc4aa947c237f8417d272aea9e7
Daniel Conlon <danielcconlon@gmail.com>
2022-08-12 23:49:08 -0400

    thought routes are working, thoughts are not showing up for a user though

---
+++ controllers/thought-controller.js
@@ -0,0 +1,67 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought with that id!' })
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // create a new thought
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // update a thought by id
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+        // delete a user
+        deleteThought({ params }, res) {
+            Thought.findOneAndDelete({ _id: params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+        }
+
+};
+
+module.exports = thoughtController
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller');
+
+// setup GET all and POST at /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+
+// set up GET one, PUT, and DELETE at /api/thoughts
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+module.exports = router;
