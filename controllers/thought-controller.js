4f255f85b5e442a043eb9943c5df77d57b34d669
Daniel Conlon <danielcconlon@gmail.com>
2022-08-13 12:16:59 -0400

    added reaction code to controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -74,6 +74,34 @@ const thoughtController = {
                 res.json(dbThoughtData);
             })
             .catch(err => res.status(400).json(err));
+        },
+
+        // create a reaction
+        createReaction({ params, body }, res) {
+            Thought.findByIdAndUpdate(
+                { _id: params.thoughtId },
+                { $push: { reactions: body } },
+                { new: true, runValidators: true }
+            )
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this ID!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+        },
+
+        // delete a reaction
+        deleteReaction({ params }, res) {
+            Thought.findOneAndUpdate(
+                { _id: params.thoughtId },
+                { $pull: { reactions: { reactionId: params.reactionId } } },
+                { new: true }
+            )
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.json(err));
         }
 
 };
